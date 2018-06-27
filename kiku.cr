4f391baaa37cd1e6fff180b8507164ce7b5c5f0c
Spencer King <spencerking@users.noreply.github.com>
2018-06-26 21:56:09 -0500

    Color output

--- kiku.cr
+++ kiku.cr
@@ -1,12 +1,13 @@
 require "mastodon"
+require "colors"
 
 client = Mastodon::Streaming::Client.new(url: "INSTANCE NAME HERE", access_token: "ACCESS TOKEN HERE")
 
 client.public do |object|
   case object
   when Mastodon::Entities::Status
-  	puts object.account.username + " " + object.account.display_name
-  	puts object.created_at.to_local
+  	puts "@".green + object.account.username.green + " " + object.account.display_name.green
+  	puts object.created_at.to_local.to_s.blue
     puts object.content.gsub(/<[^>]*>/) {}
     puts ""
   when Int32
